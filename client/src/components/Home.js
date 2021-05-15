076e6e4ddbf34dbe0a2f1489f4fd6298211b0ff0
Jayant Khandelwal <jayant.khandelwal7@gmail.com>
2021-05-15 13:02:10 +0530

    home page text changed

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Col, Container, Jumbotron } from "reactstrap";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Col, Container, Jumbotron } from 'reactstrap';
 
 function Home() {
   return (
-    <Jumbotron className="bg-cover text-white main-jumbo">
-      <Container className="main-container">
-        <h1 class="display-4">Hello, world!</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-outline-info btn-lg" href="/recommendation" role="button">&nbsp; Get Recommendation &nbsp;<i class="far fa-lightbulb"></i></a>
+    <Jumbotron className='bg-cover text-white main-jumbo'>
+      <Container className='main-container'>
+        <h1 class='display-4'>
+          <b>Learn Without Limits!</b>
+        </h1>
+        <p class='lead'>ECDB, Your Go-To Platform to Learn from the BEST!</p>
+        <hr class='my-4' />
+        <h4>
+          Take the next step toward your personal and professional goals with
+          ECDB.
+        </h4>
+        <a
+          class='btn btn-outline-info btn-lg'
+          href='/recommendation'
+          role='button'
+        >
+          &nbsp; Get Recommendation &nbsp;<i class='far fa-lightbulb'></i>
+        </a>
       </Container>
     </Jumbotron>
   );
