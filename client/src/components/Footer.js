a91d678daba1258fc81aa3b36cb6a023bb1ad024
Ayush Rathi <ayushrathi982@gmail.com>
2021-05-16 18:33:11 +0530

    Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Container, Row } from 'reactstrap';
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="footer-maindiv">
+      <Container>
+        <Row className="footer-row">
+          <i class="fas fa-envelope"></i> 
+          <i class="fab fa-linkedin"></i>
+          <i class="fab fa-facebook"></i>
+          <i class="fab fa-instagram-square"></i>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Footer;
